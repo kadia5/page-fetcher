fffe83350d869c4f5cab09cbaff2760781a0b038
kadia5 <kdabraeux@gmail.com>
2021-07-29 02:20:26 +0000

    fetcher.js

---
+++ fetcher.js
@@ -0,0 +1,30 @@
+const fs = require('fs')
+
+const content = 'Some content!'
+
+const FETCH = fs.writeFile('/w2/d3/fetcher.js', content, err => {
+  if (err) {
+    console.error(err)
+    return
+  }
+  //file written successfully
+  if (file) {
+    console.log('body:', body);
+    return
+  }
+  if (file) {
+    console.log('body:', body);
+    return
+  }
+  
+})
+FETCH;
+// request('http://www.example.edu ', (error, response, body) => {
+//   console.log('error:', error); // Print the error if one occurred
+//   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+//   console.log('body:', body); // Print the HTML for the Google homepage.
+module.export = {
+
+FETCH,
+
+}
